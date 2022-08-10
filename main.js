82486a831ced925d471562718698f2ac9a8c91a4
ScriptedDaniel <105588970+ScriptedDaniel@users.noreply.github.com>
2022-08-10 13:43:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-var Content;
-var recognition = new SpeechRecognition();
-
-function start()
-{
-    recognition.start();
-} 
-
-
+Webcam.set(
+    {
+        width: 350,
+        height: 300,
+        image_format: 'png',
+        png_quality: 90
+    }
+);
 camera = document.getElementById("camera");
-Webcam.set({
-    width:500,
-    height:400,
-    image_format : 'jpeg',
-    jpeg_quality:90
-});
+Webcam.attach('#camera');
 
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
+    });
+};
 
+console.log("ml5 Version: ",ml5.version);
+classifier = ml5.imageClassifier('i forgot',modelLoaded);
 
-function speak(){
+function modelLoaded() {
+    console.log("Model Loaded! Successfully");
+};
 
-    
-    var synth = window.speechSynthesis;
-    Webcam.attach(camera);
+function check() {
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResult);
+}
 
-    speak_data = "Taking your next Selfie in 5 seconds";
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-
-
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_of_objects").innerHTML = results[0].label;
+        document.getElementById("result_of_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
